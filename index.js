5c29e0f047542d043d84738e9c0a6b399088e422
harika <harikatrr@gmail.com>
2020-05-30 20:20:22 -1200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var mobile = {}
+module["exports"] = mobile;
+mobile.mobile_brand = require("./mobile_brand");
+mobile.android_version = require("./android_version");
+mobile.storage_capacity = require("./storage_capacity");
+mobile.ram = require("./ram");
